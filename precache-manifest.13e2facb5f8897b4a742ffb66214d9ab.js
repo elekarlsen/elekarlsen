7f1d2122af162a20a746b30c3b51d9db3b4a307e
elena karlsen <elena.karlsen@yahoo.com>
2018-12-05 16:37:06 +0100

    Updates

---
+++ precache-manifest.13e2facb5f8897b4a742ffb66214d9ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b05cc8b0c0595ea0399",
+    "url": "/website/static/css/main.90780141.chunk.css"
+  },
+  {
+    "revision": "0b05cc8b0c0595ea0399",
+    "url": "/website/static/js/main.0b05cc8b.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/website/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3927267eca3968ff588d89f3a6eacba3",
+    "url": "/website/index.html"
+  }
+];
